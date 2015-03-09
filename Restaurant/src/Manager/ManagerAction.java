46634f9e6ccaf1bca7df1f2811a9d9bae3f26de6
mad11017 <mad11017@ITESTA50.ad.engr.uconn.edu>
2015-03-09 17:31:37 -0400

    ManagerAction bill/tax

--- Restaurant/src/Manager/ManagerAction.java
+++ Restaurant/src/Manager/ManagerAction.java
@@ -1,5 +1,44 @@
 package Manager;
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+
 
 public class ManagerAction {
+	
+	private int _total;
+	private int _tax;
+	private int _bill,_tip;
+	private String _customerName,_familyName;
+	
+	public static void main(String args[]){
+		
 
+		
+	Scanner user_input=new Scanner(System.in);
+	int _total;
+	_total=user_input.next();
+	String _familyName;
+	
+
+	}
+	
+	
+	
+	
+	
+
+
+	
+	public void calculateBill(){
+		
+		_bill=_tax+_total;
+		_tip= (int) (_bill * (0.2));
+		System.out.println("The total is: " +_bill);
+		System.out.println("The suggested tip is: " +_tip);
+				
+		JOptionPane.showMessageDialog ( 
+				   null, "Put your message here" );
+		
+	}
 }
+
