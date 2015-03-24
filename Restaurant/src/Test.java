6d3566e9dc28a1e6488d03e8735a74df70661b26
chl12003 <chl12003@ITESTA58.ad.engr.uconn.edu>
2015-03-24 18:57:29 -0400

    Dragging and wood texture w/ Madhu

--- Restaurant/src/Test.java
+++ Restaurant/src/Test.java
@@ -1,5 +1,8 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 //Madhu: We should rename this 'table' and 'tableTest' 
@@ -11,13 +14,14 @@ public class Test {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       
 
-        frame.pack();
+       // frame.pack();
         frame.getContentPane().setBackground(new Color(200,100,30));
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setLayout(null);
         frame.setVisible(true);
         
+        /*
         String input = "";
-        
         String rex = "^[1-9]\\d*$";
        
         do{
@@ -25,35 +29,74 @@ public class Test {
         }while(!input.matches(rex));
         
         int number = Integer.parseInt(input);
+        */
         
-        for(int i = 0; i < number; i++)
-        {
-        	frame.getGraphics().drawRect((i*205), 0, 200, 200);
-        }
+        JPanel mainp = new JPanel();
+        mainp.setBounds(50,50,100,100);
+        mainp.setBackground(Color.CYAN);
+      
+        JPanel maind = new JPanel();
+        maind.setBounds(200,50,100,100);
+        maind.setBackground(Color.CYAN);
         
-        frame.setVisible(true);
+       // frame.getContentPane().add(mainp);
+        frame.getContentPane().add(new Table(10,10));
+        frame.getContentPane().add(new Table(10,200));
+        frame.getContentPane().add(new Table(10,200));
+        frame.repaint();
         
 	}
 }
 
-class Table extends JPanel {
-	private int _x, _y;
-
+class TableMouseListener extends javax.swing.event.MouseInputAdapter{
+	private JPanel _table;
+	private Point _lastMousePosition;
 	
+	public TableMouseListener(JPanel aPanel)
+	{
+		_table = aPanel;
+	}
+	
+	public void mousePressed(java.awt.event.MouseEvent e){
+	    _lastMousePosition=e.getPoint();
+	}
+	
+	public void  mouseDragged(java.awt.event.MouseEvent e){
+	       
+	    	   Point currentPoint=e.getPoint();
+	    	   int diffX=currentPoint.x-_lastMousePosition.x;
+	    	   int diffY=currentPoint.y-_lastMousePosition.y;
+	           _table.setLocation(_table.getX()+diffX,_table.getY()+diffY);
+	           _table.repaint();
+	           }
+	}
+
+class Table extends JPanel {
+
+	private int xClick, yClick;
+	private Image img;
 		public Table(int x, int y)
 		{
-			_x = x;
-			_y = y;
+			try{
+			img = ImageIO.read(new File("H:\\baf.jpg"));
+			}catch(Exception e)
+			{
+				
+			}
+			
+			this.setBounds(x,y,100,100);
+			this.setBackground(Color.CYAN);
+			TableMouseListener ml=new TableMouseListener(this);
+			  this.addMouseListener(ml);
+			  this.addMouseMotionListener(ml);
 		}
 		
-	  protected void paintComponent(Graphics g) {
-	    super.paintComponent(g);  
-	    g.drawRect(_x,_y,50,50);  
-	    g.setColor(Color.BLACK);  
-	    g.setColor(Color.RED);
-	    g.fillRect(_x,_y,50,50);  
-	  }
-	}
+		@Override
+		  protected void paintComponent(Graphics g) {
+		    super.paintComponent(g);
+		        g.drawImage(img, 0, 0, null);
+		}
+}
 
 
 
